45fab055c56a40f1cf913b401d18767d5ac5cd88
ledhcg <dinhcuong.firewin99@gmail.com>
2020-09-30 22:54:31 +0300

    Update fix Prak5.11

--- java_test/src/app/Prak5/Exercise11.java
+++ java_test/src/app/Prak5/Exercise11.java
@@ -28,6 +28,6 @@ public class Exercise11 {
         }
     }
     public static void main(String[] args) {
-        System.out.println(recursion()); 
+        System.out.println("Result: " + recursion()); 
     }
 }
